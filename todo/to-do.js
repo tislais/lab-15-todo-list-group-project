25ab803af025e33dbc07b8af625aef52677d223f
kayla schoorl <kaylaschoorl@gmail.com>
2021-04-15 15:21:21 -0700

    added two new functions including one in todo js

--- todo/to-do.js
+++ todo/to-do.js
@@ -1,13 +1,20 @@
 // import { }
 
+import { addToDo, getUser } from '../local-storage-utils.js';
+
 const form = document.querySelector('form');
 const toDoUl = document.getElementById('to-do-ul');
+const user = getUser();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const data = new FormData(form);
-    
     const toDoItem = data.get('to-do-item');
-    
+    addToDo(toDoItem);
+    user.todo.forEach(todo => {
+        const toDoLi = document.createElement('li');
+        toDoLi.textContent = toDoItem;
+        toDoUl.append(toDoLi);
+    })
 });
